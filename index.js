5d13a81f4859a921d01559eca6494a5c664a9f80
emilioMarchi <em.98.nmynm@gmail.com>
2022-06-06 20:06:20 -0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require('express')
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const authRoute = require('./routes/auth')
 
 const app = express()
-const port = process.env.PORT || 8080 // Heroku will need the PORT environment variable
+const port = process.env.PORT || 8080 
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Conexión a Base de datos
+
+const uri = `mongodb+srv://admin:${process.env.PASSWORD}@cluster0.1mh2ak2.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+
+// import routes
+app.use('/admin', authRoute)
+
+
+
+// route middlewares
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funciona!'
+    })
+});
+
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 
---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+const router = require('express').Router()
+const User = require('../models/user')
+
+const Joi = require('@hapi/joi');
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+router.post('/registrer', async (req, res) => {
+
+    // validate user
+    const { error } = schemaRegister.validate(req.body)
+    
+    if (error) {
+        return res.status(400).json(
+            {error: error.details[0].message}
+        )
+    }
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        res.json({
+            error: null,
+            data: savedUser
+        })
+    } catch (error) {
+        res.status(400).json({error})
+    }
+})
+
+module.exports = router
