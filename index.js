a47ad7c91bb54d273fbf9777ac197124a6869f9e
emilioMarchi <em.98.nmynm@gmail.com>
2022-06-14 21:15:08 -0300

    nuevo commit

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
-const express = require('express')
-const path = require('path');
+const express = require('express') 
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
+const path = require('path')
 require('dotenv').config()
+const cors = require('cors');
 
 
 
 const app = express()
-const port = process.env.PORT || 8080 
+const port = process.env.PORT || 8080
 
 //
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
+let corsOptions = {
+    origin: 'http://localhost:3000', // Reemplazar con dominio
+    optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
+
+
 // Conexión a Base de datos
 
 const uri = `mongodb+srv://admin:${process.env.PASSWORD}@cluster0.1mh2ak2.mongodb.net/?retryWrites=true&w=majority`;
-mongoose.connect(uri,
+const connectDb = async ()=>{ 
+    await mongoose.connect(uri,
     { useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    .then(() => console.log('Base de datos conectada'))
-    .catch(e => console.log('error db:', e))
+)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
+}
+connectDb()
     
+//import routes
+const authRoute = require('./routes/auth')
+const dashboardRoutes = require('./routes/dashboard');
+const verifyToken = require('./mdlw/validateToken');
     
-    
-    //import routes
-    const authRoute = require('./routes/auth')
-    const dashboadRoutes = require('./routes/dashboard');
-    const verifyToken = require('./mdlw/validateToken');
-    
-    app.get('/', (req, res) => {
-        res.json({
-            estado: true,
-            mensaje: 'funciona!'
-        })
-    });
+ 
 
-    // routes
-    app.use('/auth', authRoute)
-    
-    // route middlewares
-    app.use('/api/dashboard', verifyToken, dashboadRoutes);
-    
-    
+// routes
+app.use('/auth', authRoute)
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+    res.send('holis')
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+// route middlewares
+app.use('/api/dashboard', verifyToken, dashboardRoutes);
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
